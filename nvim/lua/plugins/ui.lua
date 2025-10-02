1c9c8fc31f4a2ddf1d7eb389ad5cf32271d968d8
Francisco Almeida <franciscocfalmeida@gmail.com>
2025-10-02 15:10:22 +0100

    Added noice.nvim plugin

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -41,4 +41,38 @@ return {
 			{ "<C-n>", "<cmd>Neotree toggle<cr>", desc = "Toggle Neotree" },
 		},
 	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+		opts = {
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+				},
+			},
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+			messages = {
+				enabled = true,
+				view = "mini",
+				view_error = "mini",
+				view_warn = "mini",
+				view_history = "mini",
+			},
+			notify = {
+				enabled = true,
+				view = "mini",
+			},
+		},
+	},
 }
