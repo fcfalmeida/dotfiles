4577be59a7d1c516cf674e8a1e03a5dbe193d23a
Francisco Almeida <franciscocfalmeida@gmail.com>
2025-11-18 16:52:35 +0000

    Updated oil.nvim config to show hidden files

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -20,7 +20,11 @@ return {
 		"stevearc/oil.nvim",
 		---@module 'oil'
 		---@type oil.SetupOpts
-		opts = {},
+		opts = {
+			view_options = {
+				show_hidden = true,
+			},
+		},
 		dependencies = { { "nvim-tree/nvim-web-devicons" } },
 		lazy = false,
 		keys = {
