d72408fcf7669fa669eb71511549f323d8d8d659
Francisco Almeida <franciscocfalmeida@gmail.com>
2026-02-24 18:16:03 +0000

    Added kanagawa-paper nvim theme

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -8,5 +8,16 @@ return {
 	},
 	{
 		"rebelot/kanagawa.nvim",
+		config = function()
+			require("kanagawa").setup({
+				transparent = true,
+			})
+		end,
+	},
+	{
+		"thesimonho/kanagawa-paper.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
 	},
 }
