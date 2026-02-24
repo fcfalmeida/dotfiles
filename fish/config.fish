1fa278c84b0466bcb55ae50bb8b6ed609d97e007
Francisco Almeida <franciscocfalmeida@gmail.com>
2026-02-24 18:17:11 +0000

    Added bun, protoc and opencode to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,7 @@ fish_add_path ~/go/bin/
 fish_add_path ~/software/nvim/bin
 fish_add_path /usr/local/texlive/2024/bin/x86_64-linux
 fish_add_path ~/software/vcpkg
+fish_add_path ~/software/protobuf/bin
 
 set -Ux VCPKG_ROOT ~/software/vcpkg
 set -Ux EDITOR nvim
@@ -20,3 +21,10 @@ test -f $__fish_config_dir/user.fish; and source $__fish_config_dir/user.fish
 if type -q nvm
     nvm use latest &> /dev/null
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# opencode
+fish_add_path /home/franciscoalmeida/.opencode/bin
